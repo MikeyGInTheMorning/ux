d0a7bda9c949dfefbc99882b27424e79d1c3698c
Michael <mikegulik@gmail.com>
2022-09-20 23:37:00 -0400

    cleared out unimportant and added card

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let title: string | undefined = undefined;
+
+	console.log(title);
+</script>
+
+<div class=" w-full p-5 ">
+	<div class="flex flex-col rounded shadow-2xl p-5">
+		{#if title !== undefined}
+			<div class="flex justify-between w-full">
+				<div class="mb-4 font-bold text-2xl">{title}</div>
+				<div>
+					<slot name="actions" />
+				</div>
+			</div>
+			<hr />
+		{/if}
+		<div class="flex flex-col w-full p-5 ">
+			<slot name="body" />
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Navbar from '$lib/components/navbar.svelte';
 	import 'uno.css';
 	import '@unocss/reset/tailwind.css';
+	import Card from '$lib/components/card.svelte';
 
 	// let cats: any[] = [];
 
@@ -17,19 +18,21 @@
 </script>
 
 <div class="h-100vh flex">
-	<div class="w-1/7vw">
+	<div class="w-85">
 		<div class="fixed">
 			<Navbar />
 		</div>
 	</div>
 	<div class="w-full">
-		<Footer />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
+		<Card title="Menu Items">
+			<div class="flex gap-2" slot="actions">
+				<button class="bg-blue-400 hover:bg-blue-300 active:bg-blue-500 w-120px h-40px rounded raised">Add</button>
+				<button class="bg-green-400 hover:bg-green-300 active:bg-green-500 w-120px h-40px  rounded raised">Save</button>
+			</div>
+			<div class="flex flex-col w-full" slot="body">
+				grid goes here
+				<!-- <Footer /> -->
+			</div>
+		</Card>
 	</div>
 </div>
